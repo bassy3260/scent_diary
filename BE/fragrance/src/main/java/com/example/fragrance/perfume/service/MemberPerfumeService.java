2a2ed77189a3b7ca1f056b37b57b6efc95fefa35
차경빈 <sabizupa@gmail.com>
2026-03-16 17:34:49 +0900

    chore/#96: 안 쓰는 import 삭제

--- BE/fragrance/src/main/java/com/example/fragrance/perfume/service/MemberPerfumeService.java
+++ BE/fragrance/src/main/java/com/example/fragrance/perfume/service/MemberPerfumeService.java
@@ -1,14 +1,6 @@
 package com.example.fragrance.perfume.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.example.fragrance.perfume.dto.MemberPerfumeDto;
 import com.example.fragrance.perfume.dto.MemberPerfumeListResponse;
-import com.example.fragrance.perfume.mapper.MemberPerfumeMapper;
-
-import lombok.RequiredArgsConstructor;
 
 public interface MemberPerfumeService {
 	MemberPerfumeListResponse getOwnedPerfumes(Long memberId, int page, int size);
