62f9eccf6b34a1c78d8f4f5283bc7a1d3c7eb9ee
공윤정 <mulkwhale@naver.com>
2026-03-17 17:47:37 +0900

    feat/#149: 향수 소장하기

--- BE/fragrance/src/main/java/com/example/fragrance/perfume/service/MemberPerfumeService.java
+++ BE/fragrance/src/main/java/com/example/fragrance/perfume/service/MemberPerfumeService.java
@@ -5,5 +5,6 @@ import com.example.fragrance.perfume.dto.MemberPerfumeListResponse;
 public interface MemberPerfumeService {
 	MemberPerfumeListResponse getOwnedPerfumes(Long memberId, int page, int size);
 	void deleteMemberPerfume(Long memberPerfumeId, Long memberId);
+	String insertCollect(Long memberId, Long perfumeId);
 }
 
