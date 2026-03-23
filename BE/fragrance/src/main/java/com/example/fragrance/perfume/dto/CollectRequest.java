8bbfb2a48ede92e8216ffbed2e57bc4d1ef25520
차경빈 <sabizupa@gmail.com>
2026-03-23 15:06:44 +0900

    fix/#198: 찜/소장에서 언더바-카멜케이스 매칭

--- BE/fragrance/src/main/java/com/example/fragrance/perfume/dto/CollectRequest.java
+++ BE/fragrance/src/main/java/com/example/fragrance/perfume/dto/CollectRequest.java
@@ -1,10 +1,13 @@
 package com.example.fragrance.perfume.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CollectRequest {
+	@JsonProperty("perfume_id")
     private Long perfumeId;
 }
